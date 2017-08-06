f9eda4d165d53a955db589b8bd6cba579987bf7f
Patrick Prescott <pwprescott@gmail.com>
2017-08-06 08:44:00 -0600

    test some random colors

--- Hand-Bell-Signal.ino
+++ Hand-Bell-Signal.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
+#define PANELPIN 6
 
+Adafruit_NeoPixel panel = Adafruit_NeoPixel(56, PANELPIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  // Initialize the panel
+  panel.begin();
+  panel.show();
+
+  // show some random colors
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  random();
+  delay(250);
 }
+
+void random(){
+  for( i=0;i<56;i++){
+    panel.setpixelcolor(random(254), random(254, random(254));
+  }
+  panel.show();
+}
+
